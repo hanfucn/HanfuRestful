5697c5833c838c846e472303f4cf8078c3d8fc75
张珏敏 <1748011755@qq.com>
2019-06-18 23:09:30 +0800

    声明文件GPL3.0许可

--- app/serializer.py
+++ app/serializer.py
@@ -1,3 +1,22 @@
+'''
+
+Copyright (C) 2019 张珏敏.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+'''
+
 from rest_framework import serializers
 
 from . import models
@@ -7,7 +26,9 @@ class ArticleSerializer(serializers.ModelSerializer):
     '''
     序列化文章模型
     '''
+
     class Meta:
         model = models.Article
         fields = '__all__'
+
     pass
